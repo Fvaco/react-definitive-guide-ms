a4efed1907862f04b106396c36e86d134c734eda
Iván Hernández Ortega <ivan@wupplier.com>
2018-12-07 16:06:33 +0000

    :sparkles: Assignment 3 - finished

---
+++ assignment-3/src/components/NotFound/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const notFound = () => (
+  <>
+    <h1>Page not found</h1>
+    <p>This route doesn't match any page</p>
+  </>
+);
+
+export default notFound;
--- assignment-3/src/containers/Course/Course.js
+++ assignment-3/src/containers/Course/Course.js
@@ -1,14 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Course extends Component {
-    render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
-    }
+  state = {
+    title: ""
+  };
+
+  componentDidMount() {
+    this.parseQueryString();
+  }
+  componentDidUpdate() {
+    this.parseQueryString();
+  }
+  parseQueryString = () => {
+    const query = new URLSearchParams(this.props.location.search);
+    const title = query.get("title");
+    this.setState(prevState => {
+      if (prevState.title !== title) {
+        return {
+          title
+        };
+      }
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <p>You selected the Course with ID: {this.props.match.params.id}</p>
+      </div>
+    );
+  }
 }
 
 export default Course;
